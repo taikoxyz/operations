07afc7c16ff7606517dbe65c93832288c6763dda
Jeffery Walsh <cyberhorsey@gmail.com>
2023-05-01 15:32:29 -0700

    askja provers

---
+++ askja-provers/pay.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const ethers = require("ethers");
+
+const abi = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+async function main() {
+    const data = JSON.parse(fs.readFileSync("./output.json"));
+    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_ENDPOINT);
+
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+    const contract = new ethers.Contract(USDC_ADDRESS, abi, wallet);
+   
+
+    data.map(async prover => {
+        const amount = ethers.utils.formatUnits(prover.amount, 6);
+        const tx = await contract.transfer(prover.address, amount);
+        await tx.wait(3); // wait for 3 confs?
+        prover.sent = true;
+    });
+}
+main().then(() => console.log("done")).catch(e => console.error(e));
