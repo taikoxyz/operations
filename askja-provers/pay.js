318224fa5ed9a5c52269755fc529eacc6289740b
Jeffery Walsh <cyberhorsey@gmail.com>
2023-05-03 19:40:07 -0700

    set nonce

--- askja-provers/pay.js
+++ askja-provers/pay.js
@@ -33,15 +33,18 @@ async function main() {
     const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
     const contract = new ethers.Contract(USDC_ADDRESS, abi, wallet);
    
-
-    data.map(async (prover, i) => {
+    const nonce = await wallet.getTransactionCount();
+    await Promise.all(data.map(async (prover, i) => {
         if(prover.sent) return;
+        nonce  = nonce + i;
         const amount = ethers.utils.formatUnits(prover.amount, 6);
-        const tx = await contract.transfer(prover.address, amount);
+        const tx = await contract.transfer(prover.address, amount, {
+            nonce: nonce
+        });
         await tx.wait(3); // wait for 3 confs?
         prover.sent = true;
         data[i] = prover;
         fs.writeFileSync("./output.json", JSON.stringify(data));
-    });
+    }));
 }
 main().then(() => console.log("done")).catch(e => console.error(e));
